793f11c23a1437f06b0ab9929806dac3688b1763
marcobeffa <marcobeffa@gmail.com>
2023-03-30 10:49:13 +0200

    corretto

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json 
+
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    sign_up(resource_name, resource) if resource.persisted?
+
+    if resource.persisted?
+      token = JWT.encode({user_id: resource.id}, Rails.application.credentials.devise_jwt_secret_key)
+      render json: { user: resource, token: token }
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,10 @@
 class SessionsController < Devise::SessionsController
   respond_to :json 
+  def create
+    resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+
+    token = JWT.encode({user_id: resource.id}, Rails.application.credentials.devise_jwt_secret_key)
+    render json: { user: resource, token: token }
+  end
 end
